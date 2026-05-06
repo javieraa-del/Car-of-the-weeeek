b35fd65022225463f5e0d4f0c0313ac0ce775a9a
javieraa-del <javier.aa@iesvillanueva.es>
2026-05-06 08:41:24 +0200

    Add files via upload

---
+++ script (4) (1).js
@@ -0,0 +1,5 @@
+alert("Esto es carsknowledge (CKL), pagina donde encontraras todo del motor.");
+
+let nombre=prompt("¿Cual es tu nombre?");
+
+document.getElementById("nombre-visitante").textContent = "espero que disfrutes, " + nombre ;
